f09a8e3a04758c6fd79ac37b1b796966885af420
hironmoy14 <144107059+hironmoy14@users.noreply.github.com>
2025-03-24 21:41:29 +0530

    Integrated code mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import CampaignBuilder from './CampaignBuilder/CampaignBuilder'
 import { ReactFlowProvider } from '@xyflow/react'
+import EmailEditor from './CampaignBuilder/EmailEditor'
 
 function App() {
   return (
     <ReactFlowProvider>
       <div style={ { width: "100vw", height: "100vh", position: "relative" } }>
-        <CampaignBuilder />
+        {/* <CampaignBuilder /> */}
+        <EmailEditor />
       </div>
     </ReactFlowProvider>
   )
